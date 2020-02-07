d1454f77f347af6ace2216c093f5f05ae23533eb
KJTempas <fw6927mm@go.minneapolis.edu>
2020-02-07 10:09:51 -0600

    finishedLab6part1

--- wk6/ISS/fetch.js
+++ wk6/ISS/fetch.js
@@ -1,17 +1,13 @@
 //js file for ISS location
 
 let url='https://api.wheretheiss.at/v1/satellites/25544'
-
+//link to html elements
 let issLat=document.querySelector('#iss-lat')
 let issLong = document.querySelector('#iss-long')
+let time = document.querySelector('#time')
 var issMarker //leaflet marker
 var update = 10000 //update the map every 10 seconds
-//var icon= L.icon({
-    //iconURL:'noun_iss_956251.png',
-    //iconURL: 'iss.png',
-    //iconSize: [20, 20],
-    //iconAnchor: [25,25]
-//})
+
 
 // Create the map 
 let map = L.map('iss-map').setView([0, 0], 1) //center at 0,0, and max zoom out
@@ -29,7 +25,7 @@ setInterval(iss, update)  //call the iss function every {update} seconds
 function iss() {
 fetch(url)   //fetch(url) returns a Promise
 //this is the short form - using arrow notation
-    .then(res=> res.json())  //shorter version
+    .then(res=> res.json())  
     //console.log(response)
     .then( issData => {
     console.log(issData)
@@ -39,20 +35,20 @@ fetch(url)   //fetch(url) returns a Promise
         let long =issData.longitude
        issLat.innerHTML = lat
        issLong.innerHTML = long
+
 //use the lat-long from the API call to draw a marker
      
-var icon = new L.Icon({iconUrl: 'iss.png',
+var icon = new L.Icon({iconUrl: 'noun_space station_1217219.png',
                     iconSize: [20,20],
                     iconAnchor: [25,25],
                 });
 
-//let issMarker = L.marker([lat, long]).addTo(map)//, {icon: icon}).addTo(map)//create the marker
-let issMarker = L.marker([lat, long], {icon: icon}).addTo(map)//create the icon on the map    
-//if(!issMarker) {
-         //   issMarker = L.marker([lat, long],{icon: icon}).addTo(map)//create the marker 
-        //}else{
-          // issMarker.setLatLng([lat,long]) //already exists - move to new location    
-        //}
+
+if(!issMarker) {  //if the marker/icon does not exist
+           issMarker = L.marker([lat, long],{icon: icon}).addTo(map)//create the marker 
+        }else{
+           issMarker.setLatLng([lat,long]) //if marker already exists - move it to the new location    
+        }
         //update the time element to the current date and time
         let date = Date()
         time.innerHTML = date
@@ -61,6 +57,7 @@ let issMarker = L.marker([lat, long], {icon: icon}).addTo(map)//create the icon 
         console.log(err)
     })
 }
+//space station icon by Setyo Ari Wibowo from the Noun Project
 
 
 
